9e748e652af9d6da9a415d88839f03aacf3567a6
sergeome <sergeome@gmail.com>
2018-01-06 09:17:42 -0600

    update(boilerplate): latest Angular version 5.0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-import '../../public/css/style.css';
+
+import '../assets/css/styles.css';
+
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-
 export class AppComponent { }
